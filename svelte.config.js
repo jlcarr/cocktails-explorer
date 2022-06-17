2ca39fcb96586ee029f4ee5c17d42d3bc3397d84
tristan-robinette <tristan-robinette@hotmail.ca>
2022-06-17 17:10:56 +0100

    new adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
 
 import preprocess from "svelte-preprocess";
 
-//import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-netlify';
 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
 	},
 	preprocess: [
         preprocess({
