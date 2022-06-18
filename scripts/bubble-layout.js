fe4a8eb4e8a3926a18ed040e4448a0f28c742ab5
Joseph Carr <joseph.carr@mail.mcgill.ca>
2022-06-18 17:02:45 -0400

    Refactored the visualiztion code to use the preprocessed dataset

--- scripts/bubble-layout.js
+++ scripts/bubble-layout.js
@@ -1,28 +1,60 @@
 // Force-directed graph map of IBA official cocktails
 
+var bubble_graph = null;
 var bubble_simulation = null;
+
+var bubble_colors = d3.scaleOrdinal(d3.schemePaired);
 var radius = 100;
 
-// alcohol_colors = {'Gin':'red', 'Vodka':'blue', 'Whiskey':'yellow', 'White rum':'green', 'Triple Sec': 'orange', 'Tequila':'brown', 'Cognac':'violet'};
-// alcohol_colors.hasOwnProperty(n.primary_alcohol) ? alcohol_colors[n.primary_alcohol] : 'lightblue'
-var color = d3.scaleOrdinal(d3.schemePaired);
+var ingredients_visibility = "hidden";
 
-function bubble_layout_graph(){
-	console.log(bubble_recipe_graph);
+
+function parseRecipeGraph(){
+	bubble_graph = {
+		// Extract the drinks and ingredients
+		nodes: cocktail_data.recipes.map(recipe =>
+			({
+				id: recipe.name,
+				type: 'drink',
+				img_url: recipe.image_url,
+				abv: recipe.insights.abv,
+				primary_alcohol: recipe.insights.primary_alcohol
+			})
+		).concat(cocktail_data.ingredients.map(ingredient =>
+			({
+				id: ingredient.name,
+				type: 'ingredient',
+				img_url: ingredient.image_url
+			})
+		)),
+		// Extract the links from drinks to ingredients with amounts as the weight.
+		links: cocktail_data.recipes.flatMap(recipe =>
+			recipe.ingredients.map(ingredient =>
+				({
+					source: recipe.name,
+					target: ingredient.name,
+					value: ingredient.amount
+				})
+			)
+		)
+	};
+}
+
+
+function bubbleLayoutGraph(){
+	parseRecipeGraph();
+	console.log(bubble_graph);
+	top_alcoholic_ingredients.forEach(ingredient => bubble_colors(ingredient));
+	
+	// Set up the graph in D3
 	var svg_handle = d3.select("#bubble-img");
 	var width = parseInt(svg_handle.attr("width"));
 	var height =  parseInt(svg_handle.attr("height"));
-	
-	bubble_simulation = d3.forceSimulation()
-		.force("link", d3.forceLink().id(d => d.id))
-		.force("center", d3.forceCenter(width / 2, height / 2));
-		//.force("charge", d3.forceManyBody().strength(0.00001));
 
-	// Set up the graph in D3
 	var link = svg_handle.append("g")
 		.attr("class", "links")
 		.selectAll("line")
-		.data(bubble_recipe_graph.links)
+		.data(bubble_graph.links)
 		.enter().append("line")
 		.attr("stroke-width", l => l.value)
 		.attr("visibility", "hidden");
@@ -30,52 +62,59 @@ function bubble_layout_graph(){
 	var node = svg_handle.append("g")
 		.attr("class", "nodes")
 		.selectAll("g")
-		.data(bubble_recipe_graph.nodes)
+		.data(bubble_graph.nodes)
 		.enter().append("g");
 
 	var circles = node.append("circle")
 		.attr("r", n => n.type == "drink" ? radius * Math.sqrt(n.abv) : 25)
-		.attr("fill", n => color(n.primary_alcohol))
-		.attr("visibility", n => n.type == "drink" ? "visible" : "hidden");
+		.attr("fill", n => bubble_colors(n.primary_alcohol))
+		.attr("visibility", n => n.type == "drink" ? "visible" : ingredients_visibility);
 		
 		
 	var clips = node.append("clipPath")
-		.attr('id', n => "clipping-"+n.id.replace(/[\W_]+/g, '-'))
+		.attr('id', n => "clipping-"+n.id.replace(/[\W_]+/g, ''))
 		.append("circle")
 		.attr("r", n => n.type == "drink" ? radius * Math.sqrt(n.abv) : 25)
 	var images = node.append("image")
 		.attr('opacity', 0.3)
-		.attr('href', "https://www.thecocktaildb.com/images/media/drink/rptuxy1472669372.jpg")
+		.attr('href', n => n.img_url)
 		.attr('x', n => n.type == "drink" ? -radius * Math.sqrt(n.abv) : -25)
 		.attr('y', n => n.type == "drink" ? -radius * Math.sqrt(n.abv) : -25)
-		.attr('height', n => n.type == "drink" ? 2*radius * Math.sqrt(n.abv) : 25)
-		.attr('width', n => n.type == "drink" ? 2*radius * Math.sqrt(n.abv) : 25)
-		.attr('clip-path', n => "url(#clipping-"+ n.id.replace(/[\W_]+/g, '-') +")")
-		.attr("visibility", n => n.type == "drink" ? "visible" : "hidden");
+		.attr('height', n => n.type == "drink" ? 2*radius * Math.sqrt(n.abv) : 50)
+		.attr('width', n => n.type == "drink" ? 2*radius * Math.sqrt(n.abv) : 50)
+		.attr('clip-path', n => "url(#clipping-"+ n.id.replace(/[\W_]+/g, '') +")")
+		.attr("visibility", n => n.type == "drink" ? "visible" : ingredients_visibility);
 
 	var node_text = svg_handle.append("g")
 		.attr("class", "nodes")
 		.selectAll("g")
-		.data(bubble_recipe_graph.nodes)
+		.data(bubble_graph.nodes)
 		.enter().append("g");
 
 	var lable_shadow = node_text.append("text")
 		.text(n => n.id)
 		.attr('text-anchor', "middle")
 		.attr('style', "stroke: white; stroke-width: 4; stroke-linejoin : round; opacity: 0.9;")
-		.attr("visibility", n => n.type == "drink" ? "visible" : "hidden");
+		.attr("visibility", n => n.type == "drink" ? "visible" : ingredients_visibility);
 	var lables = node_text.append("text")
 		.text(n => n.id)
 		.attr('text-anchor', "middle")
-		.attr('style', "opacity: 1.0;")
-		.attr("visibility", n => n.type == "drink" ? "visible" : "hidden");
+		//.attr('style', "opacity: 1.0;")
+		.attr("visibility", n => n.type == "drink" ? "visible" : ingredients_visibility);
+
+
+	// Set up simulation
+	bubble_simulation = d3.forceSimulation()
+		.force("link", d3.forceLink().id(d => d.id))
+		.force("collision", d3.forceCollide(n => n.type == "drink" ? radius * Math.sqrt(n.abv) : 0).iterations(1))
+		.force("center", d3.forceCenter(width / 2, height / 2))
+		.force("gravity", d3.forceManyBody().strength(1));
 
 	bubble_simulation
-		.nodes(bubble_recipe_graph.nodes)
+		.nodes(bubble_graph.nodes)
 		.on("tick", ticked);
-
 	bubble_simulation.force("link")
-		.links(bubble_recipe_graph.links)
+		.links(bubble_graph.links)
 		.strength(l => l.value); // + (l.source.primary_alchol == l.target.id)
 
 	function ticked() {
@@ -89,8 +128,6 @@ function bubble_layout_graph(){
 		node_text
 			.attr("transform", n => "translate("+ n.x + "," + n.y + ")");
 	}
-	
-	bubble_simulation.force("collision", d3.forceCollide(n => n.type == "drink" ? radius * Math.sqrt(n.abv) : 0).iterations(1));
 	
 	
 	bubble_simulation.on('end', () => {
--- scripts/cocktails-data.js
+++ scripts/cocktails-data.js
@@ -1,84 +1,36 @@
 // Force-directed graph map of IBA official cocktails
 
-var recipe_data = null;
-var ingredient_data = null;
-
-var recipe_graph = null;
-var bubble_recipe_graph = null;
+var cocktail_data = null;
+var recipe_index_map = null;
+var ingredient_index_map = null;
 
 function fetchIBADataset(){
-	var recipe_data_url = "https://raw.githubusercontent.com/teijo/iba-cocktails/master/recipes.json";
-	var ingredient_data_url = "https://raw.githubusercontent.com/teijo/iba-cocktails/master/ingredients.json";
-
-
-	var recipe_promise = d3.json(recipe_data_url)
+	var cocktail_data_url = "cocktail-insights.json";
+	
+	return d3.json(cocktail_data_url)
 	.then(data => {
-		recipe_data = data;
+		cocktail_data = data;
 	});
-	var ingredient_promise = d3.json(ingredient_data_url)
-	.then(data => {
-		ingredient_data = data;
-	});
-
-	return Promise.allSettled([recipe_promise, ingredient_promise]);
 }
 
 function preprocessData(){
-	// process volume
-	recipe_data.forEach(drink => {
-		drink.volume = drink.ingredients.reduce( (acc, ingredient) =>
-			acc + (ingredient.amount > 0 ? ingredient.amount : 0)
-		, 0)
-	});
-	
-	// process total alcohol
-	recipe_data.forEach(drink => {
-		drink.alcohol = drink.ingredients.reduce( (acc, ingredient) =>
-			acc + (ingredient.amount > 0 ? ingredient.amount * ingredient_data[ingredient.ingredient].abv / 100 : 0)
-		, 0)
-	});
-	
-	// primary alcohol
-	recipe_data.forEach(drink => {
-		drink.primary_alcohol = drink.ingredients
-			.filter(item => item.amount > 0)
-			.sort((a,b) => a.amount * ingredient_data[a.ingredient].abv < b.amount * ingredient_data[b.ingredient].abv ? 1 : -1)[0].ingredient
-	});
-
-	parseRecipeGraph();
-	
-	bubble_recipe_graph = JSON.parse(JSON.stringify(recipe_graph));
-	//filterOnlyAlcohol();
+	recipe_index_map = parseIndexMap(cocktail_data.recipes, recipe => recipe.name);
+	ingredient_index_map = parseIndexMap(cocktail_data.ingredients, ingredient => ingredient.name);
+	top_alcoholic_ingredients = cocktail_data
+		.ingredients.filter(ingredient => ingredient.abv > 0)
+		.sort((a,b) => a.insights.recipe_usage.length < b.insights.recipe_usage.length ? 1 : -1)
+		.map(ingredient => ingredient.name);
 }
 
-function parseRecipeGraph(){
-	recipe_graph = {
-		// Extract the drinks and ingredients, remove undefined special items, then sort for de-duping
-		nodes: recipe_data.flatMap(drink =>
-			[{id:drink.name, type: 'drink', abv: drink.alcohol / drink.volume, primary_alcohol: drink.primary_alcohol}].concat(
-				drink.ingredients.map(ingredient =>
-					({id: ingredient.ingredient, type: 'ingredient'})
-		)))
-			.filter(item => item.id !== undefined)
-			.sort((a,b) => a.id < b.id ? -1 : 1)
-			.filter((item, pos, arr) => !pos || item.id != arr[pos - 1].id),
-		// Extract the links from drinks to ingredients with amounts as the weight.
-		links: recipe_data.flatMap(drink =>
-			drink.ingredients.flatMap(ingredient =>
-				({source: drink.name, target: ingredient.ingredient, value: ingredient.amount / drink.volume})
-		))
-			.filter(item => item.source !== undefined && item.target !== undefined)
-	}
-}
-
-function filterOnlyAlcohol(){
-	recipe_graph.nodes = recipe_graph.nodes.filter(item => item.type != 'ingredient' || ingredient_data[item.id].abv > 0);
-	recipe_graph.links = recipe_graph.links.filter(item => ingredient_data[item.target].abv > 0);
+function parseIndexMap(arr, key_func){
+	var result = {};
+	for (var i = 0; i < arr.length; i++)
+		result[key_func(arr[i])] = i;
+	return result;
 }
 
 function layout(){
 	fetchIBADataset()
 	.then(preprocessData)
-	.then(bipartite_layout_graph)
-	.then(bubble_layout_graph);
+	.then(bubbleLayoutGraph);
 }
